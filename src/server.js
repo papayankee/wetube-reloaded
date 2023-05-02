b345e85abeb8839ab817398da9a579d0aba34ecc
pyglee <realmanpaul@gmail.com>
2023-05-03 03:10:27 +0900

    Start

---
+++ src/server.js
@@ -0,0 +1,7 @@
+ import express from "express";
+
+ const app = express();
+
+ const handleListening = () => console.log("Listening on http://localhost:4000 😘");
+
+app.listen(4000, handleListening);
