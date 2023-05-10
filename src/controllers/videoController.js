5ff91abec94fc247b43d47c982f15902082e42c8
pyglee <realmanpaul@gmail.com>
2023-05-11 01:27:10 +0900

    Squashed commit of the following:
    commit c7b0d6543d9d332d926fed0e54da353027fc55cc
    Author: pyglee <realmanpaul@gmail.com>
    Date:   Thu May 11 01:26:05 2023 +0900
    
        fucking space
    
    commit cc152fa1b81757c82b88c3ff3d7203f80db3dea5
    Author: pyglee <realmanpaul@gmail.com>
    Date:   Thu May 11 01:23:22 2023 +0900
    
        still not working

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -25,10 +25,14 @@ let videos = [
   },
 ];
 
-export const trending = (req, res) => { 
-    return res.render("home", { pageTitle: "Home", videos });
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch");
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
- import express from "express";
- import morgan from "morgan";
- import globalRouter from "./routers/globalRouter";
- import userRouter from "./routers/userRouter";
- import videoRouter from "./routers/videoRouter";
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
@@ -15,8 +15,6 @@ app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
-
-const handleListening = () => 
-    console.log(`Listening on http://localhost:${PORT} 😘`);
-
+const handleListening = () =>
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 app.listen(PORT, handleListening);
