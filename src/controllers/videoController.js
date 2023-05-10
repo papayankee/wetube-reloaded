bed41882449bfaaee79249b7483dae36451c8375
pyglee <realmanpaul@gmail.com>
2023-05-11 00:57:48 +0900

    why is this no work

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,35 @@
-const fakeUser = {
-    username: "Pyung",
-    loggedIn: false,
-};
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
 
-export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch Videos" });
-export const edit = (req, res) => res.render("edit", { pageTitle: "Edit Videos" });
+export const trending = (req, res) => { 
+    return res.render("home", { pageTitle: "Home", videos });
+};
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-    console.log(req.params);
-    res.send("Delete Video");
-}
+export const deleteVideo = (req, res) => res.send("Delete Video");
