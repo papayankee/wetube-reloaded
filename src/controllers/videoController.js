7edb80bdc593edb0bd016d8e203a989790991f22
pyglee <realmanpaul@gmail.com>
2023-05-16 05:55:11 +0900

    6.18

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -20,7 +20,8 @@ import Video from "../models/Video"
 */
 export const home = async(req, res) => {
     const videos = await Video.find({});
-    return res.render("home", { pageTitle: "Home", videos:[] });
+    console.log(videos);
+    return res.render("home", { pageTitle: "Home", videos });
 };
       
 export const watch = (req, res) => {
@@ -41,7 +42,20 @@ export const getUpload = (req, res) => {
   return res.render("upload", {pageTitle: "Upload Video"});
 };
 
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  return res.redirect("/");
-};
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try {
+    await Video.create({
+      title,
+      description,
+      createdAt: Date.now(),
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch(error){
+    return res.render("upload", {
+      pageTitle: "Upload Video", 
+      errorMessage: error._message
+    });
+  }
+  };
