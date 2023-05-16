2d9fb8b46c8b3171a969a4be297e571555cfc45f
pyglee <realmanpaul@gmail.com>
2023-05-16 16:11:36 +0900

    6.22

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,16 +1,15 @@
 import express from "express";
 import {
     watch, 
+    getUpload,
     getEdit, 
     postEdit,
-    getUpload,
     postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
-
 export default videoRouter;
