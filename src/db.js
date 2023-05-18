34813d1046c84a6e50b3527e1c302093e0543f52
pyglee <realmanpaul@gmail.com>
2023-05-19 02:25:52 +0900

    7.1

--- src/db.js
+++ src/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
     useNewUrlParser: true, 
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    //필수는 아니지만 업데이트된 내용을 적용하기 위해 사용
+    useCreateIndex: true,
 });
 
 const db = mongoose.connection;
