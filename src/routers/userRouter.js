dd5c543d0ec190c7f3fb71a3a2e84f4db2b5f65f
pyglee <realmanpaul@gmail.com>
2023-05-24 05:41:16 +0900

    7.22

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import {
     edit,
-    remove,
     logout,
     see,
     startGithubLogin,
@@ -10,9 +9,8 @@ import {
 
 const userRouter = express.Router();
 
-userRouter.get("logout", logout);
+userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/delete", remove);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/callback", finishGithubLogin);
 userRouter.get(":id", see);
